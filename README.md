5dabb011e72104ebf896c535b88b872c0af1dd1c
Gabriel R <42207245+vexuas@users.noreply.github.com>
2021-01-06 07:38:13 +0800

    Setting up ririsu (#1)
    * Install discord.js
    
    * Update readme
    
    * Add timeline
    
    * Prettify

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # ririsu-discord
+
 Reminder bot for releases
+
+Ririsu | v0.1.0 -> v1.0.0
+
+---
+
+- Create function to pick a random set of users
+- Create function to determine the following week's rotation
+- Get and list all available users in a channel
+- Add/Remove users in the pool
+- Add daily reminder function; accepts 12/24 hour clock (only Admins)
+- Ability to toggle reminder
+- Create embed styling
+- Create reply to channel functions
+- Create about command
+- Create info command
---
+++ ririsu.js
@@ -0,0 +1,19 @@
+import Discord from 'discord.js';
+import { defaultPrefix, token } from './config/ririsu.json';
+
+const ririsu = new Discord.Client();
+
+/**
+ * Ririsu v0.1.0 - v1.0.0
+ * ---------------------------------
+ * Create function to pick a random set of users
+ * Create function to determine the following week's rotation
+ * Get and list all available users in a channel
+ * Add/Remove users in the pool
+ * Add daily reminder function; accepts 12/24 hour clock (only Admins)
+ * Ability to toggle reminder
+ * Create embed styling
+ * Create reply to channel functions
+ * Create about command
+ * Create info command
+ */
